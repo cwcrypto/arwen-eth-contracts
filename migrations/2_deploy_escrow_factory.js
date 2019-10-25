49d4529f658f37be595e3645ba83ee80b261d08c
sebby <lipmas93@gmail.com>
2019-10-25 12:31:30 -0400

    [Tests] Remove truffle migration code
    + Directly deploy contracts from tests instead of relying on the
    deployed contract existing.
    
    + Use hard-coded constant for gas limit need to deploy a new factory
    (which is higher than truffle's default gas limit)

--- migrations/2_deploy_escrow_factory.js
+++
@@ -1,7 +0,0 @@
-const EscrowFactoryWithERC20 = artifacts.require("EscrowFactoryWithERC20");
-const EscrowFactory = artifacts.require("EscrowFactory");
-
-module.exports = function(deployer) {
-    var factory = deployer.deploy(EscrowFactory, { gas: 9000000});
-    var factoryWithERC20 = deployer.deploy(EscrowFactoryWithERC20, { gas: 9000000});
-};
